f9ba4916c2f95c04a7cbec46505120b32653b4e6
Mike Morais <53874401+lkazumi@users.noreply.github.com>
2023-08-31 15:44:01 -0300

    Create ConexaoAS400.js
    modelo para conectar em um banco db2 as400

---
+++ ConexaoAS400.js
@@ -0,0 +1,43 @@
+const config = {
+    host: host-database,
+    user: 'user-database',
+    password: 'password-database',
+  };
+  
+  // instalar lib abaixo
+  // npm i node-jt400
+
+  // Criando uma pool de conexões com o banco de dados
+  const dbPool = require('node-jt400').pool(config);
+  
+  // Função para executar a consulta
+  function executarConsulta() {
+    // Consulta SQL
+    const sqlQuery = "SELECT AM01_ARTNAM  FROM BASDB01.AM01 WHERE AM01_ARTNUM = ?";
+    const params = [3074];
+  
+    // Executando a consulta usando a pool de conexões
+    dbPool
+      .query(sqlQuery, params)
+      .then(result => {
+        console.log('Resultado da consulta:');
+        console.log(result);
+  
+        /*/ Extraindo os valores do resultado
+        if (result.length > 0) {
+          const field1 = result[0].FIELD1;
+          console.log('Valor do field1:', field1);
+        }*/
+
+        
+      dbPool.close();
+      })
+      .catch(error => {
+        console.error('Erro ao executar a consulta:');
+        console.error(error);
+      });
+  }
+  
+  // Chamando a função para executar a consulta
+  executarConsulta();
+  
